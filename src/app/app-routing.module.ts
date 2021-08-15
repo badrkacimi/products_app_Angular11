e123287a396fc4972c34c8d60565150c0c374bb4
Badr Kacimi <badrvkacimi@gamail.com>
2021-08-15 23:16:11 +0100

    Fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
+import {ProductEditComponent} from "./components/product-edit/product-edit.component";
+import {ProductAddComponent} from "./components/product-add/product-add.component";
 
 const routes: Routes = [
-{ path : 'products', component: ProductsComponent},
-{ path : 'home'    , component: HomeComponent},
-
+  { path: "products", component:ProductsComponent},
+  { path: "newProduct", component:ProductAddComponent},
+  { path: "editProduct/:id", component:ProductEditComponent},
+  { path: "", component:HomeComponent},
 ];
 
 @NgModule({
